6c4c3bbac55efd85374b8ccf4ed077da93aafa29
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-06-30 12:31:39 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  const selectDifferentDrivers = function(drivers, fn) {
+    return fn(drivers);
+  };
+  const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+console.log(returnFirstTwoDrivers(drivers));
+console.log(returnLastTwoDrivers(drivers)); 
+
+console.log(selectingDrivers[0](drivers)); 
+console.log(selectingDrivers[1](drivers)); 
+
+const doubler = fareDoubler(10);
+console.log(doubler); 
+
+const tripler = fareTripler(10);
+console.log(tripler); 
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); // Output: ['Antonia', 'Nuru']
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers)); // Output: ['Amari', 'Mo']
